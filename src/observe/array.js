a95dd2a2058ae8e86c001438e2da5f9fe8d029ee
dingming <lol>
2023-02-04 22:38:10 +0800

    save

---
+++ src/observe/array.js
@@ -0,0 +1,41 @@
+let oldArrayProto = Array.prototype
+
+export let newArrayProto = Object.create(oldArrayProto)
+
+// 数组的变异方法
+let methods = [
+    'push',
+    'pop',
+    'shift',
+    'unshift',
+    'reserve',
+    'sort',
+    'splice',
+] //concate slice 都不会改变原数组
+
+methods.forEach(method=>{
+    newArrayProto[method]=function(...args){
+        const result = oldArrayProto[method].call(this,...args) //???  this
+        
+        
+        console.log(method)
+        let inserted
+        switch (method) {
+            case value:
+                case 'push':
+                case 'unshift':
+                    inserted = args
+                    break;
+                case 'splice':  //splice 是第三个参数往后才是插入的内容
+                    inserted = args.slice(2)
+        
+            default:
+                break;
+        }
+        console.log(inserted) 
+        if(inserted){
+            
+        }
+        return result
+    }
+})
--- src/observe/index.js
+++ src/observe/index.js
@@ -1,22 +1,34 @@
+import { newArrayProto } from "./array"
+
 class Observer{
+    
     constructor(data){
-        this.walk(data)
+        
+        if(Array.isArray(data)){
+            data.__proto__ = newArrayProto  //如果是数组，先重写原型链上的push，ushift之类的方法。
+            this.observeArray(data)
+        }else{
+            this.walk(data)
+        }     
     }
     walk(data){
         Object.keys(data).forEach(key=>{
             return defineReactive(data,key,data[key])
         })
     }
+    observeArray(data){
+        data.forEach(item=>observe(item))
+    }
 }
 export function defineReactive(target,key,value){
     observe(value) //如果是嵌套了对象，再劫持一遍 （是否对象的判断在observe函数里存在了，所以这里无需判断）
     Object.defineProperty(target,key,{
         get(){
-            // console.log('get')
+            // console.log('getkey',key)
             return value
         },
         set(newValue){
-            // console.log('set')
+            // console.log('setkey',key)
             if(newValue === value) return
             observe(newValue)  // 比如： vm.address = {num:20}
             value = newValue
