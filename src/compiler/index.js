10a62ea49f6287a2ab4cd385fd2116c054c9856e
dingming <lol>
2023-02-05 20:51:30 +0800

    正则

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,5 +1,23 @@
-export function complileToFunction(){
+const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z]*`;
+const qnameCapture = `((?:${ncname}\\:)?${ncname})`;
+const startTagOpen = new RegExp(`^<${qnameCapture}`); // 他匹配到的分组是一个 标签名  <xxx 匹配到的是开始 标签的名字
+const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`);  // 匹配的是</xxxx>  最终匹配到的分组就是结束标签的名字
+const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/;  // 匹配属性
+// 第一个分组就是属性的key value 就是 分组3/分组4/分组五
+const startTagClose = /^\s*(\/?)>/;  // <div> <br/>
+
+function parseHTML(html){
+    while(html){
+        
+    }
+}
+
+
+
+export function complileToFunction(template){
     // 1.template -> ast 语法树
     // 2.生成render函数（render函数返回虚拟dom）
-    console.log('compile')
+    console.log(template)
+
+    let ast = parseHTML(template)
 }
